8dc61fc8859597611c54fad9b95c062f01ec4765
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2020-10-24 16:09:00 -0500

    Add cuda version, Add targets to cmake

--- loop1_cuda.cu
+++ loop1_cuda.cu
@@ -8,12 +8,17 @@
 #define BUF_TYPE float
 #endif
 
-inline float to_float(int in) {
-    return (float) in;
+#define CudaWrap(EXP) \
+{ \
+    auto ret = EXP; \
+    if (ret != cudaSuccess) { \
+        std::cerr << "Error! " << cudaGetErrorString(ret) << " (" << ret << ")" << std::endl; \
+        return 1; \
+    }\
 }
 
 __global__
-void addKernel(const float* A, const float* B, float* C, size_t size) {
+void addKernel(const BUF_TYPE* A, const BUF_TYPE* B, BUF_TYPE* C, size_t size) {
     size_t i = blockIdx.x*blockDim.x+threadIdx.x;
     if (i < size) {
         C[i] = A[i] + B[i];
@@ -24,19 +29,24 @@ int main() {
     // Initialize random number generator
     std::mt19937_64 generator;
     generator.seed(42);
-    std::uniform_real_distribution<float> distribution(-1., 1.);
+    std::uniform_real_distribution<BUF_TYPE> distribution(-1., 1.);
     auto gen = std::bind(distribution, generator);
 
     // Initialize arrays
-    size_t num_gen = 1<<(30-2);
+    size_t num_gen = (1<<(30))/sizeof(BUF_TYPE);
 
-    float* array1;
-    float* array2;
-    float* array3;
+    // Allocate arrays.
+    BUF_TYPE* array1 = new BUF_TYPE[num_gen];
+    BUF_TYPE* array2 = new BUF_TYPE[num_gen];
+    BUF_TYPE* array3 = new BUF_TYPE[num_gen];
 
-    cudaMallocManaged(&array1, num_gen*sizeof(float));
-    cudaMallocManaged(&array2, num_gen*sizeof(float));
-    cudaMallocManaged(&array3, num_gen*sizeof(float));
+    // Allocate Device arrays.
+    BUF_TYPE* dev_1 = nullptr;
+    BUF_TYPE* dev_2 = nullptr;
+    BUF_TYPE* dev_3 = nullptr;
+    CudaWrap(cudaMalloc(&dev_1, num_gen*sizeof(BUF_TYPE)));
+    CudaWrap(cudaMalloc(&dev_2, num_gen*sizeof(BUF_TYPE)));
+    CudaWrap(cudaMalloc(&dev_3, num_gen*sizeof(BUF_TYPE)));
 
     // Fill arrays with values
     for(size_t i=0; i < num_gen; ++i) {
@@ -46,13 +56,21 @@ int main() {
 
     auto start = std::chrono::high_resolution_clock::now();
 
+    // Copy data to Device
+    cudaMemcpy(dev_1, array1, num_gen*sizeof(BUF_TYPE), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_2, array2, num_gen*sizeof(BUF_TYPE), cudaMemcpyHostToDevice);
+    // Compute on Device
     addKernel<<<1,1>>>(array1, array2, array3, num_gen);
+    // Copy data out of Device
+    cudaMemcpy(dev_3, array3, num_gen*sizeof(BUF_TYPE), cudaMemcpyDeviceToHost);
+    // Wait for operations to finish.
     cudaDeviceSynchronize();
 
     auto stop = std::chrono::high_resolution_clock::now();
 
+
     // Do something with the arrays so the addition isn't optimized out.
-    float sum = 0.;
+    BUF_TYPE sum = 0.;
     for(size_t i=0; i < num_gen; ++i) {
         sum += array3[i];
     }
@@ -65,9 +83,13 @@ int main() {
 
     std::cout << "Took " << duration.count() << " milliseconds" << std::endl;
 
-    cudaFree(array1);
-    cudaFree(array2);
-    cudaFree(array3);
+    cudaFree(dev_1);
+    cudaFree(dev_2);
+    cudaFree(dev_3);
+
+    delete [] array1;
+    delete [] array2;
+    delete [] array3;
 
     return 0;
 }
